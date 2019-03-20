425a5352f7fb92a819be5d4e8f6fdba4855aa3c2
Ngozi Emeh-Festus <zingo1ng@yahoo.com>
2019-03-20 10:23:54 +0100

    Update codebase.js

--- codebase.js
+++ codebase.js
@@ -7,3 +7,4 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   
   //1. Generate random number
   var dice1 = Math.floor(Math.random() * 6) + 1;
+  var dice2 = Math.floor(Math.random() * 6) + 1;
