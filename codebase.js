baf5f3fc13f025b34a4da6e21a7db3beee93c351
Ngozi Emeh-Festus <zingo1ng@yahoo.com>
2019-03-20 11:05:43 +0100

    Update codebase.js

--- codebase.js
+++ codebase.js
@@ -84,5 +84,6 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     
   });  
   
+// DOM Manipulation for game restart button  
 document.querySelector('.btn-new').addEventListener('click', function(){  
     
